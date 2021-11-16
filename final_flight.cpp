9f668183449bc6f2b3e1b120c0a945b645156f41
yaJ <propilir0908@gmail.com>
2021-11-16 10:45:18 +0800

    Updating seat flight

--- final_flight.cpp
+++ final_flight.cpp
@@ -7,6 +7,7 @@ string destination, flightNum, flightName, timeDeparture, plane_class, email="",
 int user_decision, price, total, ticketCount,userChoice, seatAvailable, nDiscount;
 float change, input_money, balance=0, discount;
 char option;
+int seatAvailable1 = 0, seatAvailable2 = 15, seatAvailable3 = 8, seatAvailable4 = 24, seatAvailable5 = 7, seatAvailable6 = 13;
 void showMainMenu()
 {
     cout << "|------------------------------------|" << endl;
@@ -24,18 +25,18 @@ void showMainMenu()
 }
 void showAvailableFlights()
 {
-
+    //0 15 8 24 7 13
 	cout <<  "|----------------------------------------------------------------------------------------------------------------------|" << endl;
 	cout <<  "| Flight Destination | Flight No.  | Flight Name  | Time of Departure | Class        |Price           |Seat Available  |" << endl;
     cout <<  "|----------------------------------------------------------------------------------------------------------------------|" << endl;
-	cout <<  "|  South Korea    [1]|   001       |  SK001       | 0700h             | Economy      |  7,000 Php     |       0        |" << endl;
-	cout <<  "|                 [2]|   002       |  SK002       | 1300h             | Premium Eco. |  10,000 Php    |       15       |" << endl;
+	cout <<  "|  South Korea    [1]|   001       |  SK001       | 0700h             | Economy      |  7,000 Php     |       "<<seatAvailable1 << "        |" << endl;
+	cout <<  "|                 [2]|   002       |  SK002       | 1300h             | Premium Eco. |  10,000 Php    |       "<<seatAvailable2 << "       |" << endl;
 	cout <<  "|----------------------------------------------------------------------------------------------------------------------|" << endl;
-	cout <<  "|  Hongkong       [3]|   003       |  HK003       | 0900h             | Economy      |  10,000 Php    |       8        |" << endl;
-	cout <<  "|                 [4]|   004       |  HK004       | 1500h             | Premium Eco. |  14,000 Php    |       24       |" << endl;
+	cout <<  "|  Hongkong       [3]|   003       |  HK003       | 0900h             | Economy      |  10,000 Php    |       "<<seatAvailable3 << "        |" << endl;
+	cout <<  "|                 [4]|   004       |  HK004       | 1500h             | Premium Eco. |  14,000 Php    |       "<<seatAvailable4 << "       |" << endl;
 	cout <<  "|----------------------------------------------------------------------------------------------------------------------|" << endl;
-	cout <<  "|  Japan          [5]|   005       |  JPN005       | 1000h            | Economy      |  20,000 Php    |       7        |" << endl;
-	cout <<  "|                 [6]|   006       |  JPN006       | 1800h            | Premium Eco. |  30,000 Php    |       13       |" << endl;
+	cout <<  "|  Japan          [5]|   005       |  JPN005       | 1000h            | Economy      |  20,000 Php    |       "<<seatAvailable5 << "        |" << endl;
+	cout <<  "|                 [6]|   006       |  JPN006       | 1800h            | Premium Eco. |  30,000 Php    |       "<<seatAvailable6 << "       |" << endl;
     cout <<  "|----------------------------------------------------------------------------------------------------------------------|" << endl;
 }
 void displayFlight()
@@ -129,6 +130,30 @@ void showChosenFlight()
     else if (ticketCount <= seatAvailable)
     {
         total = price * ticketCount;
+        if (user_decision ==1)
+        {
+            seatAvailable1 -=ticketCount;
+        }
+        else if(user_decision==2)
+        {
+            seatAvailable2 -=ticketCount;
+        }
+        else if(user_decision==3)
+        {
+            seatAvailable3 -=ticketCount;
+        }
+        else if(user_decision==4)
+        {
+            seatAvailable4 -=ticketCount;
+        }
+        else if(user_decision==5)
+        {
+            seatAvailable5 -=ticketCount;
+        }
+        else if(user_decision==6)
+        {
+            seatAvailable6 -=ticketCount;
+        }
         cout << "The total amount is: " << total << endl << endl << endl;
         getEmail();
         cout << "To avail the discounted price for PWD/Senior Citizen press [1] if not press [2]" << endl;
@@ -186,7 +211,7 @@ void Menu()
             timeDeparture ="0700h";
             plane_class = "Economy";
             price = 7000;
-            seatAvailable = 0;
+            seatAvailable = seatAvailable1;
             showChosenFlight();
             user_decision = userChoice;
         }
@@ -198,7 +223,7 @@ void Menu()
             timeDeparture ="1300h";
             plane_class = "Premium Economy";
             price = 10000;
-            seatAvailable = 15;
+            seatAvailable = seatAvailable2;
             showChosenFlight();
             user_decision = userChoice;
         }
@@ -210,7 +235,7 @@ void Menu()
             timeDeparture ="0900h";
             plane_class = "Economy";
             price = 10000;
-            seatAvailable = 8;
+            seatAvailable = seatAvailable3;
             showChosenFlight();
             user_decision = userChoice;
         }
@@ -222,7 +247,7 @@ void Menu()
             timeDeparture ="1500h";
             plane_class = "Premium Economy";
             price = 14000;
-            seatAvailable = 24;
+            seatAvailable = seatAvailable4;
             showChosenFlight();
             user_decision = userChoice;
         }
@@ -234,7 +259,7 @@ void Menu()
             timeDeparture ="1000h";
             plane_class = "Economy";
             price = 20000;
-            seatAvailable = 7;
+            seatAvailable = seatAvailable5;
             showChosenFlight();
             user_decision = userChoice;
         }else if (user_decision == 6)
@@ -245,7 +270,7 @@ void Menu()
             timeDeparture ="1800h";
             plane_class = "Premium Economy";
             price = 30000;
-            seatAvailable = 13;
+            seatAvailable = seatAvailable6;
             showChosenFlight();
             user_decision = userChoice;
         }
